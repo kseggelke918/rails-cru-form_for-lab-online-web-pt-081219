6f8ccf4141fd5651dc5e514fade3984a83314036
Katie Seggelke <katie.seggelke@gmail.com>
2020-01-07 20:51:11 +0000

    Automatically backed up by Learn

--- app/controllers/genres_controller.rb
+++ app/controllers/genres_controller.rb
@@ -1,2 +1,24 @@
 class GenresController < ApplicationController
+  
+    def new 
+  end 
+  
+  def create 
+  end 
+  
+  def edit 
+  end 
+  
+  def update 
+  end 
+  
+  def destroy
+  end 
+  
+  def show 
+  end 
+  
+  def index 
+  end 
+  
 end
